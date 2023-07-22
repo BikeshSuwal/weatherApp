a1d27023e34a4d84f1faf974f32353ee69925696
unknown <bikeshsuwal776@gmail.com>
2023-07-22 11:43:06 +0545

    app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 
 import "./css/App.css"
 import tempImg from "./assets/thermometer.png"
+import { useState } from "react"
+import axios from "axios";
 
 
 function App() {
+  const [data,setData] = useState({});
+  const [location, setLocation] = useState('');
 
-  const url ='https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=141474aa8babcd859329a1fe178f467c'
+
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=141474aa8babcd859329a1fe178f467c`;
+
+  const searchLocation = event => {
+    // setCurrentLocation(location);
+    console.log(location);
+    if (event.key == 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(data);
+      })
+      setLocation('');
+    }
+
+  }
+
   return (
     <>
       <div className="container">
@@ -14,12 +33,15 @@ function App() {
             Weather App
           </h1>
           <div className="maincard">
-              <div className="up">fsdf
-                <div className="searchbar"></div>
+              <div className="up">
+                <div className="location">{data.name ? data.name : 'Location'}</div>
+                <div className="searchbar">
+                  <input value={location} onChange={event => setLocation(event.target.value)} placeholder="Enter Location" onKeyDown={event => searchLocation(event)} type="text" className="text" />
+                </div>
               </div>
               <div className="down">
                 <div className="des">
-                  Cloudy
+                  {data.weather ? data.weather[0].main : null}
                 </div>
                 <div className="info">
                   <div className="temp">
@@ -27,18 +49,18 @@ function App() {
                       <img src={tempImg} alt="Thermometer Image"/>
                     </div>
                     <div className="tempValues">
-                      <div className="mainTemp">30°C</div>
-                      <div className="otherTemp"><div className="minTemp"></div>Min: 26°C<div className="maxTemp">Max: 32°C</div></div>
+                      <div className="mainTemp">{data.main ? (data.main.temp-273).toFixed(1) + '°C' : null}</div>
+                      <div className="otherTemp"><div className="minTemp"></div>Min: {data.main ? (data.main.temp_min-273).toFixed(1) + '°F' : null}<div className="maxTemp">Max: {data.main ? (data.main.temp_max-273).toFixed(1) + '°F' : null}</div></div>
                     </div>
                   </div>
 
                   <div className="others">
                     <div className="pressure othersBox">
-                      <div className="dataValue">1008</div>
+                      <div className="dataValue">{data.main ? data.main.pressure : null}</div>
                       <div className="dataName">Pressure</div>
                     </div>
                     <div className="humidity othersBox">
-                      <div className="dataValue">82%</div>
+                      <div className="dataValue">{data.main ? data.main.humidity + '%' : null}</div>
                       <div className="dataName">Humidity</div>
                     </div>
                   </div>
